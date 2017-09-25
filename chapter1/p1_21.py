200d5b62fcc2d7feced6dbafed390913f9662340
findman <findman@gmail.com>
2017-09-26 00:01:31 +0800

    对不原生支持比较操作的对象排序

---
+++ chapter1/p1_21.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding:utf8 -*-
+
+class User:
+    def __init__(self, user_id):
+        self.user_id = user_id
+
+    def __repr__(self):
+        return 'User({})'.format(self.user_id)
+
+
+users = [User(23), User(3), User(99)]
+print(users)
+users = sorted(users,key=lambda u:u.user_id)
+print(users)
