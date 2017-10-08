eee0449969d351f218f52fddfe0b7f76da3e7885
findman <findman@gmail.com>
2017-10-09 05:41:46 +0800

    添加第三章

---
+++ chapter3/p01_round.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding:utf8 -*-
+
+def round_fun():
+    print(round(1.23,1))
+    print(round(1.27,1))
+    print(round(-1.27,1))
+    print(round(1.25361,3))
+    a = 16227731
+    print(round(a,-1))
+    print(round(a,-2))
+    print(round(a,-3))
+    x = 1.23456
+    print(format(x,'0.2f'))
+    print(format(x,'0.3f'))
+    print('value is {:0.3f}'.format(x))
+    a = 2.1
+    b = 4.2
+    c = a + b
+    print(c)
+    c = round(c,2)
+    print(c)
+
+
+
+if __name__ == '__main__':
+    round_fun()
